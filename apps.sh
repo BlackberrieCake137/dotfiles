1cd2c475edcf7e499f5b466bc870108031233ac9
Liz <121859020+BlackberrieCake137@users.noreply.github.com>
2025-01-24 15:42:52 -0500

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,5 +1,5 @@
 yay -S brave-bin beeper-latest-bin newsboat calc xfce3 xfce4-goodies ani-cli mullvad-vpn neovim steam linux-lts obsidian neomutt docker syncthing termusic libreoffice-fresh wttr epr-git keyguard-git flatpak mikhak-fonts thunderbird scopa xpytile-git ttf-ms-fonts inkscape python-svgwrite python-fonttools python-numpy heroic-games-launcher-bin zekr npm yt-dlp mutt-wizard-git hyprland ytfzf wikiman freetube ttf-google-fonts-git lunar-client tuimoji timer-bin strawberry vesktop-bin flatpak elinks ddgr catnap-git alacritty meow external-editor-revived youtui rclone ttf-maple-beta ttf-iosevka librewolf floorp ghostty
 
-yay -R fastfetch firedragon octopi firedragon-extension-plasma-integration kate kdeconnect
+&&
 
-sudo npm install -g quran
+yay -R fastfetch firedragon octopi firedragon-extension-plasma-integration kate kdeconnect
